4a319303988eedc64af3d088d39c22ddf471ff4d
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-01-24 08:38:06 -0800

    add find, includes, and reverse to index

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const every = require('./src/every')
 const filter = require('./src/filter')
+const find = require('./src/find')
+const includes = require('./src/includes')
 const map = require('./src/map')
+const reverse = require('./src/reverse')
 
 module.exports = {
   every,
   filter,
-  map
+  find,
+  includes,
+  map,
+  reverse
 }
-
